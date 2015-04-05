bf27f256f8bdd0e9dd41a90c2ae7033cea7b6a7e
vegizinho <miroslav.vegh@techlab.hr>
2015-04-05 23:32:21 +0200

    Title change https://app.asana.com/0/30877232020731/30877232020735

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include "core/engine.php";
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Special Web Service PHP Engine (Bootstrap 2.x)</title>
+<title>Special Web Service PHP Engine Bootstrap</title>
 
 <!-- Bootstrap -->
 
